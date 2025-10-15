172f164c7cbb0eda5a8526fe989719da731c7835
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-15 14:22:11 +0200

    on html put 2 input and 1 button on js create a var to calcolate the discount for age passenger and stamp the result in console when i write age and km on input and click the button "check" stamp it on console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 console.log("its work");
+
+const bottone = document.getElementById("calcola")
+
+bottone.addEventListener(`click`, function() {
+const km = document.getElementById("km").value;
+const eta = document.getElementById("eta").value;
+const prezzokm = 0.21;
+let prezzototale = km * prezzokm;
+
+if ( eta < 18){
+    prezzototale *= 0.8;
+} else if (eta >= 65) {
+    prezzototale *= 0.6;
+}
+
+console.log(`Prezzo totale ${prezzototale}`);
+
+})
