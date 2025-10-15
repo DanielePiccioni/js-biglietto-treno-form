5e0581a7b80b0ef8652a88e6d8224164b76186c9
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-15 15:59:02 +0200

    on HTML create a form and inside put the input and button and create a div to stamp a result. and create a id for form and div. on JS take the DOM form from id and put inside the other js write before and take the DOM div from id and stamp the result in HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,30 @@
 console.log("its work");
 
 const bottone = document.getElementById("calcola")
+const form = document.getElementById("form")
+const kmEL = document.getElementById("km");
+const etaEL = document.getElementById("eta");
 
-bottone.addEventListener(`click`, function() {
-const km = document.getElementById("km").value;
-const eta = document.getElementById("eta").value;
-const prezzokm = 0.21;
-let prezzototale = km * prezzokm;
+form.addEventListener(`submit`, function (event) {
+    event.preventDefault();
+    const km = parseInt(kmEL.value)
+    const eta = parseInt(etaEL.value)
 
-if ( eta < 18){
-    prezzototale *= 0.8;
-} else if (eta >= 65) {
-    prezzototale *= 0.6;
-}
+    const prezzokm = 0.21;
+    let prezzototale = km * prezzokm;
 
-console.log(`Prezzo totale ${prezzototale}`);
+    if (eta < 18) {
+        prezzototale *= 0.8;
+    } else if (eta >= 65) {
+        prezzototale *= 0.6;
+    }
+
+    console.log(`Prezzo totale ${prezzototale}`);
+
+    const risultato = document.getElementById("risultato");
+    risultato.innerHTML = `
+    <p>Vuoi percorrere ${km} km</p>
+    <p>hai un età di ${eta}</p>
+    <p>per un totale di ${prezzototale} €</p>`
 
 })
