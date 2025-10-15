385d685c74dda52b742f94a6797c960748298c78
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-15 16:43:46 +0200

    add other input to insert the name and surname and stamp on html

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ const form = document.getElementById("form")
 //prendo gli input dei km e dell'età
 const kmEL = document.getElementById("km");
 const etaEL = document.getElementById("eta");
+const nomeEL = document.getElementById("nome");
 
 //prende la sezione form ed impedisco di far refreshare 
 //la pagina quando clicchi il pulsante
@@ -14,6 +15,7 @@ form.addEventListener(`submit`, function (event) {
     //leggi i valori inseriti e li converte da string a numeri
     const km = parseInt(kmEL.value)
     const eta = parseInt(etaEL.value)
+    const nome = nomeEL.value;
 
     //calcolo degli sconti per eventuali differenze di età
     const prezzokm = 0.21;
@@ -30,6 +32,7 @@ form.addEventListener(`submit`, function (event) {
     //prendo il DOM del div e stampo il risultato in HTML
     const risultato = document.getElementById("risultato");
     risultato.innerHTML = `
+    <p>Il suddetto ${nome}</p>
     <p>Vuoi percorrere ${km} km</p>
     <p>hai un età di ${eta}</p>
     <p>per un totale di ${prezzototale} €</p>`
